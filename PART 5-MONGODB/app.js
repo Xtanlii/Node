599c9e2f2d902605d89e8ac3286da224affc68a5
Odoh Stanley <stanleyodoh780@gmail.com>
2025-09-23 14:14:13 +0100

    app

---
+++ PART 5-MONGODB/app.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+const key = require('./key')
+
+
+connect().catch(err => console.log(err))
+async function connect() {
+  await mongoose.connect(
+    `mongodb+srv://${key.password}@cluster0.o2zp1hm.mongodb.net/`
+  )
+  console.log('database connected successfully')
+}
+
+
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  isActive: Boolean,
+  tags: [String],
+  createdAt: { type: Date, default: Date.now }
+
+})
+
+//Create user model
+const User = mongoose.model('User', userSchema)
+
+async function runQueryExamples() {
+  try {
+    //create a new document
+    // const newUser = await User.create({
+    //   name: 'Travis Doe',
+    //   email: "travis@gmail.com",
+    //   age: 31,
+    //   isActive: true,
+    //   tags: ['developer'],
+    // }) 
+
+    /*
+  const newUser = new User({
+    name: 'Arky Simons',
+    email: "arkysimons115@gmail.com",
+    age: 21,
+    isActive: true,
+    tags: ['developer', 'designer', 'manager'],
+  })
+    await newUser.save()
+    */
+    // const allUsers = await User.find({});
+    // console.log(allUsers);
+    //  console.log('Created new user', newUser);
+
+    // const getUserOfActiveFalse = await User.find({isActive: true})
+    // console.log(getUserOfActiveFalse);
+
+    // const getJohnDoeUser = await User.findOne({name: "John Doe"})
+    // console.log(getJohnDoeUser);
+
+    // const getById = await User.findById(newUser._id);
+    // console.log(getById)
+
+    // const selectedFields = await User.find().select('name email -_id');
+    // console.log(selectedFields);
+
+    // const limitedUsers = await User.find().limit(5).skip(1)
+    // console.log(limitedUsers);
+
+    // const sortedUsers = await User.find().sort({age: 1})
+    // console.log(sortedUsers)
+
+    const countDoc = await User.countDocuments({isActive: true});
+    console.log(countDoc);
+    
+    
+
+  } catch (err) {
+    console.log('Error ->', err)
+  } finally {
+    await mongoose.connection.close()
+  }
+}
+
+runQueryExamples();
