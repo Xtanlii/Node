b1fa7db571cc8f3df7804f387f8c660cd8dd5ab1
Odoh Stanley <stanleyodoh780@gmail.com>
2025-09-25 15:30:51 +0100

    Queries for sorting and filtering

--- PART 5-MONGODB/Practice/index.js
+++ PART 5-MONGODB/Practice/index.js
@@ -15,7 +15,7 @@ const userSchema = new Mongoose.Schema({
   name: String,
   email: String,
   age: Number,
-  isAdmin: Boolean
+  isAdmin: {type: Boolean, default: false}
 })
 
 const User = Mongoose.model('Practice', userSchema)
@@ -24,27 +24,44 @@ async function main() {
   try {
     await connect().catch(err => console.log(err, err.message))
 
-    // const newUser = await User.create({
-    //   name: "Eren Yeager",
-    //   email: "suzuma322@gmail.com",
-    //   age: 26
-    // })
+    
+    const newUser = await User.create({
+      name: " Ariana",
+      email: "arianaffs3im@gmail.com",
+      age: 31
+    }) 
 
     /*const newUser = new User({
       name: "Kareem Benzema",
       email: "kareemballon007@gmail.com",
       age: 19
-    }) */
+    }) 
 
-    // await newUser.save()
+    await newUser.save() */
+
+    const allUsers = await User.find();
+    console.log(allUsers);
     // console.log('newUser created succesfully', newUser)
-    co
+    // const userWithAgeBelow25 = await User.find({age: {$lt: 25}})
+    // console.log(userWithAgeBelow25)
+    // const byEmail = await User.find({email: 'suzuma322@gmail.com'})
+    // console.log(byEmail)
+
+    // const updateUser = await User.findByIdAndUpdate('68d452cef5a867ac65004c74',  {
+    //   $set: {email: "updatedemail@gmail.com"}
+    // }, {new: true})
+    // console.log(updateUser);
+
+    // const deleteUser = await User.findByIdAndDelete('68d45fa0ee742802e6e151f8')
+    // console.log(deleteUser)
+
+
   } catch (err) {
     console.log(err.message)
   } finally {
     await Mongoose.connection.close();
   }
-  
+
 }
 
 main();
