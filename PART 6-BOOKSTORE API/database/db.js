611cf5cadaa2f0768f3b85ae51a678a15d8fbe13
Odoh Stanley <stanleyodoh780@gmail.com>
2025-09-26 22:10:49 +0100

    .

--- PART 6-BOOKSTORE API/database/db.js
+++ PART 6-BOOKSTORE API/database/db.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
+const key = process.env.KEY
+
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://xtan:xtan@cluster0.90etgtk.mongodb.net/');
+    await mongoose.connect(`mongodb+srv://${key}cluster0.90etgtk.mongodb.net/`);
     console.log('MongoDB is connected successfully !')
   } catch (error) {
     console.error('MongoDB connection failed:', error);
