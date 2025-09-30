ead57b751a3a2bcd70405f3e82f42a0645d440ce
Odoh Stanley <stanleyodoh780@gmail.com>
2025-09-30 14:50:22 +0100

    Create registration controller

--- PART 7-LOGIN PRACTICE/backend/controller/auth-controller.js
+++ PART 7-LOGIN PRACTICE/backend/controller/auth-controller.js
@@ -1,12 +1,63 @@
 const User = require('../model/user-model');
+const bcrypt = require('bcrypt');
 
 
 // Registration controller
-const register = async (req,res) => {
-  res.json({ message: 'Register endpoint' });
+const register = async (req, res) => {
+  try {
+    //get user data
+    const {username, email, password, role} = req.body;
+
+    //check if user already exists
+    const existingUser = await User.findOne({$or: [{email}, {username}]});
+    if(existingUser){
+      return res.status(400).json({
+        success: false,
+        message: "User already exists"
+      });
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //create new user
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      role
+    })
+    if(!newUser){
+      return res.status(400).json({
+        success: false,
+        message: "Something went wrong. Please try again later."
+      });
+    }
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+    });
+    
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong. Please try again later."
+    })
+  }
 };
 
-const login = async (req,res) => {
+const login = async (req, res) => {
+  try {
+    
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong. Please try again later."
+    })
+  }
 
 };
 
--- PART 7-LOGIN PRACTICE/backend/server.js
+++ PART 7-LOGIN PRACTICE/backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./database/db');
-const router = require('./routes/auth-routes');
+const authRoute = require('./routes/auth-routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,7 +10,7 @@ connectDB();
 
 //middleware
 app.use(express.json());
-app.use('/api/auth', router);
+app.use('/api/auth', authRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
