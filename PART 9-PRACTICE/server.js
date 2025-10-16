16e9e6a92ec71a317435263cd5379cbcb16fdc2e
Odoh Stanley <stanleyodoh780@gmail.com>
2025-10-16 14:49:22 +0100

    practice

--- PART 9-PRACTICE/server.js
+++ PART 9-PRACTICE/server.js
@@ -9,14 +9,40 @@ const server = http.createServer(app);
 //initialize socket and attach to http server
 const io = socketIo(server)
 app.use(express.static('public'))
+const users = new Set();
 
 
 io.on("connection", (socket) => {
-  socket.emit
+  socket.on('join', (newUser) => {
+    
+    users.add(newUser);
+    socket.newUser = newUser;
+    //sends a new  user to all the clients
+    io.emit('userJoined', newUser);
+    //sends new user list
+    io.emit('userList', Array.from(users));
+  });
+
+  //chat message
+  socket.on('newMessage', (message) => {
+    io.emit('newMessage', message)
+  })
+
+  socket.on('disconnect', () => {
+    users.forEach(user => {
+      if (user === socket.newUser) {
+        users.delete(user);
+
+        io.emit('userLeft', user);
+        io.emit('userList', Array.from(users));
+      }
+    })
+  })
+  
 })
 
 
 const PORT = 3000
 server.listen(PORT, () => {
-  `Server is listening  on PORT ${PORT}`
+  console.log(`Server is listening  on PORT ${PORT}`)
 })
